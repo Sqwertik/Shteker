32b0ab5fa46a51df6383587f508c1b79f5fa46ed
Sqwertik <topmuvik070@gmail.com>
2025-11-08 15:35:07 +0200

    New header
    better optimized for some devices and was added dark/light modes and a new burger and more some functions

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const burgerBtn = document.getElementById('burgerBtn');
 const menu = document.getElementById('menu');
+const changeItem = document.getElementById('changeItem');
 
 burgerBtn.addEventListener('click', () =>
 {
     menu.classList.toggle('open');
+    burgerBtn.classList.toggle('open');
+});
+
+
+changeItem.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    changeItem.classList.toggle('dark');
+    changeItem.classList.toggle('light');
+
+
+    changeItem.textContent = document.body.classList.contains('dark')
+    ? 'Light Mode' 
+    : 'Dark Mode';
 });
